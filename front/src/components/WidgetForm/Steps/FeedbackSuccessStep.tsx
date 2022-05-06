49b4af768d32dfa175b8cefe36d4c2333fa693ad
Bernardo Müller <95920671+Bmullerc@users.noreply.github.com>
2022-05-06 13:04:51 -0300

    Add files via upload

---
+++ front/src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
@@ -0,0 +1,30 @@
+import { CloseButton } from "../../CloseButton";
+import successImageURL from "../../../assets/success.svg"
+
+interface FeedbackSuccessStepProps {
+    onFeedbackRestartRequested: () => void;
+}
+
+export function FeedbackSuccessStep( { onFeedbackRestartRequested }: FeedbackSuccessStepProps  ) {
+    return (
+        <>
+            <header>
+                <CloseButton />
+            </header>
+
+            <div className="flex flex-col items-center py-10 w-[304px]">
+                <img src={successImageURL} />
+                <span className="text-xl mt-2">Agradecemos o feedback!</span>
+
+                <button
+                    type="button"
+                    onClick={onFeedbackRestartRequested}
+                    className="py-2 px-6 mt-6 bg-zinc-800 rounded-md border-transparent text-sm leading-6 hover:bg-zinc-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
+                >
+                    Quero enviar outro
+                </button>
+            </div>
+
+        </>
+    )
+}
